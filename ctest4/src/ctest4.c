f16c1d3d098c756be86dbc858bf498ddaeeb16de
ZIMIN WEI <zwei6@gmail.com>
2020-03-25 22:59:22 -0400

    Add ctest4

---
+++ ctest4/src/ctest4.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : ctest4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Operator in C
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    printf("%d", 1 << 2 + 3 << 4);
+    return 0;
+}
